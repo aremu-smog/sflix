795cb48773eafe812eea40182fdc8e67be8c3e57
Aremu Oluwagbamila (SMOG) <aremuoluwagbamila@gmail.com>
2019-11-06 19:06:05 +0100

    Fix conflicts

--- src/Home/Header.js
+++ src/Home/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Header = () =>{
     return(
         <header className="main-header">
             <h1>UNLIMITED ENTERTAINMENT <br/>JUST FOR YOU</h1>
-            {/* <button>Get Started Now</button> */}
+            <Link to="/signup"><button>Get Started Now</button></Link>
             <div class="landing-carousel">
                 <img src="img/posters/1.jpg" alt="" class="item" height="300px"/>
                 <img src="img/posters/2.jpg" alt="" class="item" height="300px"/>
